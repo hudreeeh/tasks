02543a8dfcacd1cceed00b10c7169659549fba63
hudreeh <sheridandamien7@gmail.com>
2022-02-16 10:59:19 +0600

    feat: finished 16.02 task

---
+++ 16.02/script.js
@@ -0,0 +1,12 @@
+// task #1
+let camelCase = 1;
+let snake_case = 2;
+let PascalCase = 3;
+alert(`${camelCase} ${snake_case} ${PascalCase}`);
+console.log(`${camelCase} ${snake_case} ${PascalCase}`);
+
+// task #2
+let str1 = 'САЛАМ';
+let str2 = ' БИШКЕК!';
+alert(`${str1}, ${str2}`);
+console.log(`${str1} ${str2}`);
