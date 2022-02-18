194b50104b0774cbccdbd46ff23d88c7bf2adac1
hudreeh <sheridandamien7@gmail.com>
2022-02-18 11:09:22 +0600

    feat: started 18.02 task

---
+++ 18.02/script.js
@@ -0,0 +1,42 @@
+// task 1
+let num = 10;
+num++;
+console.log(num);
+let num1 = 10;
+num1--;
+console.log(num1);
+
+// task 2
+let name = 'Demien';
+console.log('Привет, ' + name + '!');
+
+// task3
+let age = 19;
+console.log('Мне ' + age + ' лет!');
+
+// task4
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+// task5
+let num2 = 47;
+num2 += 7;
+console.log(num2);
+alert(num2);
+let num3 = 47
+num3 -= 18;
+console.log(num3);
+alert(num3);
+let num4 = 47;
+num4 *= 10;
+console.log(num4);
+alert(num4)
+let num5 = 47;
+num5 /= 15;
+console.log(num5);
+alert(num5);
+
