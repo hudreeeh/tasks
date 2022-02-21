394d4babb48f70cc2c8ee17df336083ce526d53c
hudreeh <sheridandamien7@gmail.com>
2022-02-21 11:37:44 +0600

    feat: finished 21.02 task

---
+++ 21.02/1/script.js
@@ -0,0 +1,28 @@
+// &&
+let a = 10;
+let b = 11;
+let result = a == 10 && b == 11;
+console.log(result);
+
+let result2 = a > 20 && b < 10;
+console.log(result2);
+
+let result3 = a == 10 && b > 12;
+console.log(result3);
+
+let result4 = a < 5 && b == 11;
+console.log(result4);
+
+// ||
+let c = 20;
+let result5 = c == 20 || c != 19;
+console.log(result5);
+
+let result6 = c >= 18 || c <= 17;
+console.log(result6);
+
+let result7 = c > 10 || c < 11;
+console.log(result7);
+
+let result8 = c > 30 || c != 20;
+console.log(result8);
---
+++ 21.02/2/script.js
@@ -0,0 +1,16 @@
+// task 1
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
+
+// task 2
+var num2 = 10;
+num2++;
+++num2;
+num2--;
+alert(num2);
