f532e2cb9398ed0f55d5a8c99a0e62b433c8ec20
hudreeh <sheridandamien7@gmail.com>
2022-02-25 11:25:50 +0600

    feat: finished 25.02.class

---
+++ 25.02.class/script.js
@@ -0,0 +1,18 @@
+
+let ques = prompt('Кто пришел?', '');
+if(ques == 'Admin'){
+    let pass = prompt('Пароль?', '');
+    if(pass == 'hello'){
+        alert('Добро пожаловать!');
+    }
+    else if(pass == null){
+        alert('Вход отменен');
+    }
+    else{
+        alert('Пароль неверен');
+    }
+} else if(ques == null){
+    alert('Вход отменен');
+} else{
+    alert('Я вас не знаю');
+}
